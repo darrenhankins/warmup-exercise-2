1b89d2081366819fa0fdd9d75ae0d971d762b7e7
Darren Hankins <hankins.darren@gmail.com>
2017-01-24 18:50:33 -0700

    updated knexfile.js to run on heroku db

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postres://localhost/warmup-exercise'
-  }
-  // },
+  },
 
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL + '?ssl=true'
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
 };
